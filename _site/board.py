525c85c375688f4e53263df30c26d6bf4e065979
Idris Raja <idris.raja@gmail.com>
2021-04-19 17:20:57 -0500

    start cleaning things up

---
+++ _site/board.py
@@ -0,0 +1,10 @@
+
+
+class Board:
+    def __init__(self, img):
+        """ img: some image cropped to the starting position of a chess board """
+        self.img = img
+
+    def parse(self):
+        """ takes self.img and gets each of the 64 squares """
+        pass
---
+++ docs/about.md
@@ -0,0 +1,11 @@
+---
+layout: default
+title: About
+---
+# About page
+
+this page tells you everything about me.
+
+## My history
+
+I was born before the start of time
