5bb8e6d320df060655c47bb40afbd2e8ab273d43
Idris Raja <idris.raja@gmail.com>
2021-05-27 07:08:21 -0500

    create package

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
 # Chess Board Detection
 
-The intent of this project is to build a model to detect chess boards in images,
-and then translate the image to a FEN representation.
-
-Also the project is to get up to speed with the world of:
-
-* fastai
-* matplotlib
-* nbdev and nbconvert
-* opencv
-* jupyter notebooks
-* github actions 
-* github projects
-* github pages
-* coreml
-* deploying models from python world to iOS world
+Utility functions for training the chess model.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chessviz",
+    version="0.0.1",
+    author="Idris Raja",
+    author_email="idris.raja@gmail.com",
+    description="A small utility package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/idrisr/chess-vision",
+    project_urls={
+        "Bug Tracker": "https://github.com/idrisr/chess-vision/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+)
