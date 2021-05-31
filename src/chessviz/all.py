ea215c1be5c2b074e0ad466595f300d1fed32f2c
Idris Raja <idris.raja@gmail.com>
2021-05-31 16:40:21 -0500

    add some funcs

--- src/chessviz/all.py
+++ src/chessviz/all.py
@@ -2,7 +2,7 @@ from fastcore.transform import *
 from fastai.data.all import *
 from fastai.vision.all import *
 
-__all__ = ['NoLabelBBoxLabeler', 'BBoxTruth', 'iou']
+__all__ = ['NoLabelBBoxLabeler', 'BBoxTruth', 'iou', 'ParentSplitter', 'NoLabelBBoxBlock']
 
 class NoLabelBBoxLabeler(Transform):
     """ Bounding box labeler with no label """
@@ -33,3 +33,19 @@ def iou(pred, target):
     intersect_area = np.maximum(ab[:, :, [2, 3]].min(axis=0) - ab[:, :, [0, 1]].max(axis=0), 0).prod(axis=1)
     union_area = ((ab[:, :, 2] - ab[:, :, 0]) * (ab[:, :, 3] - ab[:, :, 1])).sum(axis=0) - intersect_area
     return (intersect_area / union_area).mean()
+
+
+def _parent_idxs(items, name):
+    def _inner(items, name): return mask2idxs(Path(o).parent.name == name for o in items)
+    return [i for n in L(name) for i in _inner(items,n)]
+
+
+def ParentSplitter(train_name='train', valid_name='valid'):
+    "Split `items` from the grand parent folder names (`train_name` and `valid_name`)."
+    def _inner(o):
+        return _parent_idxs(o, train_name),_parent_idxs(o, valid_name)
+    return _inner
+
+
+NoLabelBBoxBlock = TransformBlock(type_tfms=TensorBBox.create, 
+                             item_tfms=[PointScaler, NoLabelBBoxLabeler])
